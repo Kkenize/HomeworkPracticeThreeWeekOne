65e34380db5bfb373d2bb986d0ffa0dad005b8ec
Kkenize <Kkenizez@gmail.com>
2025-01-19 22:29:44 -0500

    This is my one button challenge for week one!

--- HomeworkPracticeThreeWeekOne/ContentView.swift
+++ HomeworkPracticeThreeWeekOne/ContentView.swift
@@ -8,12 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image = ""
+    @State private var message = ""
+    @State private var int = 1
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Image(systemName: image)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.yellow)
+                .frame(width: 200, height: 200)
+            Text(message)
+                .bold()
+                .font(.largeTitle)
+                .foregroundStyle(.yellow)
+            Spacer()
+            Button("Let's Go!") {
+                if int % 2 == 1 {
+                    image = "sun.max"
+                    message = "You are awesome!"
+                    int += 1
+                } else {
+                    image = "hand.thumbsup"
+                    message = "You are wonderful!"
+                    int += 1
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .bold()
+            .font(.title2)
+            .tint(.blue)
         }
         .padding()
     }
